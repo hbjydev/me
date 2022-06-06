cceff7a5ff90421be1f44ad8de82b295207efe2f
Hayden Young <hayden@hbjy.dev>
2022-06-06 10:00:28 +0100

    make commit message use note title

--- zet/new.sh
+++ zet/new.sh
@@ -4,5 +4,5 @@ mkdir -p $id
 touch $id/README.md
 nvim $id/README.md
 git add $id/README.md
-git commit -m "zettel $id"
+git commit -m "$(head -n 1 $id/README.md | awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}')"
 git push -u origin main
